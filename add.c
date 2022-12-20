26b5294559280c89b91cf95655c18741cfdd374b
teylorchapman <teylor.chapman2@gmail.com>
2022-12-19 20:58:46 -0800

    fixing errors

--- add.c
+++ add.c
@@ -9,24 +9,16 @@
 
 void mon_add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *p = (*stack);
-	int len = 0;
-	int sum = 0;
+stack_t *temp = *stack;
 
-	while (p != NULL)
-	{
-		len++;
-		p = p->next;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		sum = (*stack)->n + (*stack)->next->n;
-		(*stack)->next->n = sum;
-		mon_pop(stack, line_number);
-	}
+if (!temp || !temp->next)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+free_stack(stack);
+err();
+}
+temp->next->n += temp->n;
+*stack = temp->next;
+(*stack)->prev = NULL;
+free(temp);
 }
--- swap.c
+++ swap.c
@@ -4,33 +4,19 @@
 * mon_swap - swaps the top two elements of a stack
 * @stack: linked list stack to swap
 * @line_number: current line number of the bytecode file
+* @file: ...
 */
 
 void mon_swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *p = (*stack);
-	int len = 0;
+int temp = (*stack)->n;
 
-	while (p != NULL)
-	{
-		len++;
-		p = p->next;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		stack_t *node1 = (*stack);
-		stack_t *node2 = (*stack)->next;
-
-		node1->prev = node2;
-
-		node2->prev = NULL;
-		node2->next = node1;
-
-		(*stack) = node2;
-	}
+if (!(*stack) || !(*stack)->next)
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+fclose(file);
+exit(EXIT_FAILURE);
+}
+(*stack)->n = (*stack)->next->n;
+(*stack)->next->n = temp;
 }
